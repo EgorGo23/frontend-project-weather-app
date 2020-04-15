ef832e6d0f707210e39221879a535f977490fe13
Egor <egor.go2303@gmail.com>
2020-04-15 22:02:45 +0300

    try fix city-search-wrapper

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,48 +33,35 @@ const App = () => {
 
       <main className="main-container__bg">
 
-        <div className="add-wrapper">
+        <div className="main-card">
 
+          <div className="city-search-wrapper">
 
-          <div className="main-card">
+            <div className="city-search-header">
+              <h3 className="city-search-title">SEARCH CITIES</h3>
+              <div className="search-city-input-wrapper">
+                <input className="search-city-input" auto-complete-placeholder="search city" placeholder="search city" />
 
-            <div className="city-search-wrapper">
-
-              <div className="city-search-header">
-                <h3 className="city-search-title">SEARCH CITIES</h3>
-                <div className="search-city-input-wrapper">
-                  <input className="search-city-input" auto-complete-placeholder="search city" placeholder="search city" />
-
-                  <button className="search-city-btn">
-                    <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 451 451">
-                      <path fill="#FFF"
-                        d="M447 428L337.6 318.4A192.5 192.5 0 0 0 192.4 0C86.3 0 0 86.3 0 192.3s86.3 192.3 192.3 192.3c48.2 0 92.3-17.8 126-47.2L428.2 447a13.2 13.2 0 0 0 19 0 13.5 13.5 0 0 0 0-19zM27 192.3C27 101.1 101 27 192.3 27c91.1 0 165.3 74.2 165.3 165.3s-74.2 165.4-165.4 165.4A165.6 165.6 0 0 1 27 192.3z"></path>
-                    </svg>
-                  </button>
-                </div>
-
-              </div>
-
-              <div className="city-search-body">
-                <div className="city-weather-card">
-                  <div className="weather-card">
-                      
-                  </div>
-                </div>
+                <button className="search-city-btn">
+                  <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 451 451">
+                    <path fill="#FFF"
+                      d="M447 428L337.6 318.4A192.5 192.5 0 0 0 192.4 0C86.3 0 0 86.3 0 192.3s86.3 192.3 192.3 192.3c48.2 0 92.3-17.8 126-47.2L428.2 447a13.2 13.2 0 0 0 19 0 13.5 13.5 0 0 0 0-19zM27 192.3C27 101.1 101 27 192.3 27c91.1 0 165.3 74.2 165.3 165.3s-74.2 165.4-165.4 165.4A165.6 165.6 0 0 1 27 192.3z"></path>
+                  </svg>
+                </button>
               </div>
 
             </div>
 
-
-
-            <div className="fav-city-wrapper">
+            <div className="city-search-body">
+              <div className="weather-card">
+                
+              </div>
+            </div>
+          </div>
+          <div className="fav-city-wrapper">
                           
-            </div>
-
-
           </div>
         </div>
-
       </main>
     </Fragment>
   )
