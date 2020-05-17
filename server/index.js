5fe132df5bb1a5ad5f4ec5009141759c256690bc
EgorGo23 <egor.go2303@gmail.com>
2020-05-17 20:00:24 +0300

    heroku5

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const app = express();
 const port = process.env.PORT || 4300;
 
 app.use(express.static(path.join(__dirname, '..', 'dist')));
-app.use(express.static(path.join(__dirname, '..', 'public', 'icons')));
-console.log(path.join(__dirname, '..', 'public', 'icons'));
+app.use(express.static(path.join(__dirname, '..', 'public', 'icons/animated')));
+
 /* Переменна для хранения прочитанных и форматированных данных из базы */
 let uploadedData = null;
 /* В indexRanges хранятся индексы начала и конца списка городов, начинающихся с query
